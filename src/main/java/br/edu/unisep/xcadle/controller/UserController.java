02412d512c9f68361ac49cbf6f16ee3383ca7462
eduardovanzetto <eduardovanzetto@gmail.com>
2019-11-27 21:02:45 -0300

    controller usuario

---
+++ src/main/java/br/edu/unisep/xcadle/controller/UserController.java
@@ -0,0 +1,30 @@
+package br.edu.unisep.xcadle.controller;
+
+import br.edu.unisep.xcadle.domain.dto.user.NewUserDto;
+import br.edu.unisep.xcadle.domain.dto.user.UserDto;
+import br.edu.unisep.xcadle.domain.usecase.user.ListUserUseCase;
+import br.edu.unisep.xcadle.domain.usecase.user.NewUserUseCase;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    private ListUserUseCase listUserUseCase;
+    private NewUserUseCase newUserUseCase;
+
+    @PostMapping
+    public void save(@RequestBody NewUserDto newUser) {
+        newUserUseCase.save(newUser);
+    }
+
+    @GetMapping
+    public List<UserDto> listAll() {
+        return listUserUseCase.execute();
+    }
+
+}
